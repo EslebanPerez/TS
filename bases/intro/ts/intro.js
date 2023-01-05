e86522b1296a0dc124dffbbe87f143e9591baa13
EslebanPerez <eslebanperez@gmail.com>
2023-01-05 13:41:47 -0600

    Interface en Typescript

--- bases/intro/ts/intro.js
+++ bases/intro/ts/intro.js
@@ -12,13 +12,30 @@ const drink = new Drink('Agua');
 console.log(drink.info());
 // Herencia
 class Beer extends Drink {
-    constructor(name, alcohol) {
+    constructor(name, alcohol, price) {
         super(name);
+        this.price = price;
         this.alcohol = alcohol;
+    }
+    getPrice() {
+        return "$" + this.price;
     }
     info() {
         return super.info() + ' ' + this.alcohol;
     }
 }
-const beer = new Beer("XX", 8.5);
+//Implementación de interface
+class Snack {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+    getPrice() {
+        return "El precio es: $" + this.price;
+    }
+}
+const beer = new Beer("XX", 8.5, 48);
 console.log(beer.info());
+const papitas = new Snack("papas", 40);
+console.log(papitas.name);
+console.log(papitas.getPrice());
--- bases/intro/ts/intro.ts
+++ bases/intro/ts/intro.ts
@@ -15,14 +15,25 @@ class Drink{
 const drink = new Drink('Agua')
 console.log(drink.info()); 
 
+// Interface
+interface Product {
+  price: number;
+  getPrice(): string;
+}
+
 // Herencia
-class Beer extends Drink{
+class Beer extends Drink implements Product {
   private alcohol: number;
+  price: number;
 
-  constructor(name:string, alcohol:number){
+  constructor(name:string, alcohol:number, price: number){
     super(name);
-
+    this.price = price
     this.alcohol = alcohol;
+  }
+
+  getPrice(): string{
+    return "$"+ this.price;
   }
 
   info(): string{
@@ -30,5 +41,25 @@ class Beer extends Drink{
   }
 }
 
-const beer = new Beer("XX", 8.5)
+//Implementación de interface
+class Snack implements Product{
+  name: string;
+  price: number;
+
+  constructor(name: string, price: number){
+    this.name = name;
+    this.price = price;
+  }
+  
+  getPrice(): string {
+    return "El precio es: $"+this.price;
+  }
+}
+
+
+const beer = new Beer("XX", 8.5, 48 )
 console.log(beer.info());
+
+const papitas = new Snack("papas", 40);
+console.log(papitas.name);
+console.log(papitas.getPrice());
