2a2693e9c07929f99c70e29b8142a96dde097251
EslebanPerez <eslebanperez@gmail.com>
2023-05-03 20:41:46 -0600

    Declaración de Objetos especificando sus tipos de dato

---
+++ bases/intro/objects/objects.ts
@@ -0,0 +1,14 @@
+(()=>{
+  let flash:{ name: string, age?: number, powers: string[], getName?:()=>string } = {
+    name: 'Barry Allen',
+    age: 24,
+    powers: ['Super velocidad', 'Viajar por el tiempo']
+  }
+
+  let Superman:{ name: string, age?: number, powers: string[], getName?:()=>string } = {
+    name: 'Clark Ken',
+    age: 60,
+    powers: ['Super Fuerza']
+  }
+
+})()
