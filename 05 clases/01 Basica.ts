df53877ee4aa0b065f74478c364cf2146c7eb523
EslebanPerez <eslebanperez@gmail.com>
2023-05-04 23:57:36 -0600

    Métodos públicos y privados

---
+++ 05 clases/01 Basica.ts
@@ -0,0 +1,27 @@
+(()=>{
+  class Avenger{
+    //private name:string;
+    //private team:string;
+    //public realName?: string;
+    static avgAge: number = 35;
+    static getAvgAge(){
+      return this.name;
+    }
+
+    constructor( 
+      private name:string,
+      private team:string,
+      public realName?: string,
+    ){}
+
+    public bio(){
+      return `${this.name} (${this.team})`
+    }
+  }
+
+  const antman: Avenger = new Avenger('Antman', "Captain");
+  console.log(antman);
+
+  console.log(Avenger.getAvgAge())
+    
+})()
